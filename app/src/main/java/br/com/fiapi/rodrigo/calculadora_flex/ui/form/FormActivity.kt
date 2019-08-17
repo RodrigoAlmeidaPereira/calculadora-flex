39f7989c0503e3058875af717de9300e1c398120
Rodrigo de Almeida Pereira <rodrigoalmeida.as@gmail.com>
2019-08-17 15:32:33 -0300

    Persisting data

--- app/src/main/java/br/com/fiapi/rodrigo/calculadora_flex/ui/form/FormActivity.kt
+++ app/src/main/java/br/com/fiapi/rodrigo/calculadora_flex/ui/form/FormActivity.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.fiapi.rodrigo.calculadora_flex.R
+import br.com.fiapi.rodrigo.calculadora_flex.model.CarData
 import br.com.fiapi.rodrigo.calculadora_flex.ui.result.ResultActivity
 import br.com.fiapi.rodrigo.calculadora_flex.watchers.DecimalTextWatcher
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_form.*
 
 class FormActivity : AppCompatActivity() {
@@ -17,10 +23,18 @@ class FormActivity : AppCompatActivity() {
         val ETHANOL_AVERAGE_KEY = "ETHANOL_AVERAGE"
     }
 
+    private lateinit var userId : String
+    private lateinit var mAuth: FirebaseAuth
+    private val firebaseReferenceNode = "CarData"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+
+        mAuth = FirebaseAuth.getInstance()
+        userId = mAuth.currentUser?.uid ?: ""
+        listenerFirebaseRealtime()
 
         etGasPrice.addTextChangedListener(DecimalTextWatcher(etGasPrice))
         etEthanolPrice.addTextChangedListener(DecimalTextWatcher(etEthanolPrice))
@@ -35,5 +49,40 @@ class FormActivity : AppCompatActivity() {
             nextScreen.putExtra(ETHANOL_AVERAGE_KEY, etEthanolAverage.text.toString().toDouble())
             startActivity(nextScreen)
         }
+    }
+
+    private fun saveCarData() {
+        val carData = CarData(
+            etGasPrice.text.toString().toDouble(),
+            etEthanolPrice.text.toString().toDouble(),
+            etGasAverage.text.toString().toDouble(),
+            etEthanolAverage.text.toString().toDouble()
+        )
+
+        FirebaseDatabase.getInstance()
+            .getReference(firebaseReferenceNode)
+            .child(userId)
+            .setValue(carData)
+    }
+
+    private fun listenerFirebaseRealtime() {
+        val database = FirebaseDatabase.getInstance()
+
+        database.setPersistenceEnabled(true)
+        database.getReference(firebaseReferenceNode)
+            .child(userId)
+            .addValueEventListener(object : ValueEventListener {
+
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val carData = dataSnapshot.getValue(CarData::class.java)
+                    etGasPrice.setText(carData?.gasPrice.toString())
+                    etEthanolPrice.setText(carData?.ethanolPrice.toString())
+                    etGasAverage.setText(carData?.gasAverage.toString())
+                    etEthanolAverage.setText(carData?.ethanolAverage.toString())
+                }
+
+                override fun onCancelled(p0: DatabaseError) {
+                }
+            })
     }
 }
